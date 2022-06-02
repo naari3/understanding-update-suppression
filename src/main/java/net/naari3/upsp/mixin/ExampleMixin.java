b52e3d15aae35e82a88a11b100845231111c3ef2
naari3 <naari.named@gmail.com>
2022-06-03 01:03:39 +0900

    Rename

--- src/main/java/net/naari3/upsp/mixin/ExampleMixin.java
+++ src/main/java/net/naari3/upsp/mixin/ExampleMixin.java
@@ -1,7 +1,8 @@
-package net.fabricmc.example.mixin;
+package net.naari3.upsp.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.naari3.upsp.Main;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
